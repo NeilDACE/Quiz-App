8c6f697b28a01fcfa6bb9a4aba84da28434afdc6
NilDACE <nil.vollhardt@gmail.com>
2025-11-27 18:36:22 +0100

    feat: script.js more questions added and the code is shorted and cleaner

--- script.js
+++ script.js
@@ -1,100 +1,332 @@
 const question = [
   {
-    question:
-      "Welches Pokémon der ersten Generation hat den höchsten Basis-Spezial-Angriff?",
-    answer_1: "Mewtu",
-    answer_2: "Simsala",
-    answer_3: "Dragoran",
-    answer_4: "Arktos",
-    right_answer: 1,
+    "question": "Welches Pokémon der ersten Generation hat die höchste Basis-KP (Kraftpunkte)?",
+    "answer_1": "Relaxo",
+    "answer_2": "Chaneira",
+    "answer_3": "Lapras",
+    "answer_4": "Arkani",
+    "right_answer": 2
   },
   {
-    question:
-      "Welche Attacke konnte in der ersten Generation aufgrund eines Fehlers unendlich oft eingesetzt werden?",
-    answer_1: "Wutanfall",
-    answer_2: "Verzweifler",
-    answer_3: "Genusskuss",
-    answer_4: "Genickbruch",
-    right_answer: 2,
+    "question": "Wie viele Orden muss man in der Kanto-Region (Generation 1) sammeln?",
+    "answer_1": "7",
+    "answer_2": "8",
+    "answer_3": "10",
+    "answer_4": "6",
+    "right_answer": 2
   },
   {
-    question:
-      "Welche Voraussetzung muss erfüllt sein, damit Scherox entsteht, wenn Sichlor getauscht wird?",
-    answer_1: "Es hält ein Metallmantel",
-    answer_2: "Es trägt ein Metallstaub",
-    answer_3: "Es hat maximale Zuneigung",
-    answer_4: "Es wird nachts getauscht",
-    right_answer: 1,
+    "question": "Welches der folgenden Pokémon entwickelt sich durch den Wasserstein?",
+    "answer_1": "Quaputzi",
+    "answer_2": "Evoli",
+    "answer_3": "Fukano",
+    "answer_4": "Vulpix",
+    "right_answer": 2
   },
   {
-    question:
-      "Welches Pokémon ist das einzige aus Generation 2, das eine Babyform erst in Generation 3 erhielt?",
-    answer_1: "Marill",
-    answer_2: "Woingenau",
-    answer_3: "Rabauz",
-    answer_4: "Schiggy",
-    right_answer: 2,
+    "question": "Welches der folgenden ist keines der Starter-Pokémon der ersten Generation?",
+    "answer_1": "Bisasam",
+    "answer_2": "Schiggy",
+    "answer_3": "Pikachu",
+    "answer_4": "Glumanda",
+    "right_answer": 3
   },
   {
-    question: "Wie heißt die versteckte Fähigkeit von Brutalanda?",
-    answer_1: "Hochmut",
-    answer_2: "Rohe Gewalt",
-    answer_3: "Achtlos",
-    answer_4: "Zenithaut",
-    right_answer: 1,
+    "question": "Wie wird die Fähigkeit von Glurak (Generation 1) genannt?",
+    "answer_1": "Großbrand",
+    "answer_2": "Flammenkörper",
+    "answer_3": "Feuerfänger",
+    "answer_4": "Hitzeschutz",
+    "right_answer": 1
   },
   {
-    question:
-      "Welche Voraussetzung benötigt Pupitar, um sich zu Despotar zu entwickeln?",
-    answer_1: "Level 50",
-    answer_2: "Level 55",
-    answer_3: "Level 60",
-    answer_4: "Level 65",
-    right_answer: 2,
+    "question": "Welches ist das legendäre Vogel-Trio der Kanto-Region?",
+    "answer_1": "Raikou, Entei, Suicune",
+    "answer_2": "Zapdos, Arktos, Lavados",
+    "answer_3": "Latias, Latios, Lugia",
+    "answer_4": "Mew, Mewtu, Zapdos",
+    "right_answer": 2
   },
   {
-    question:
-      "Welches legendäre Pokémon der dritten Generation ist nicht Teil des Wetter-Trios?",
-    answer_1: "Kyogre",
-    answer_2: "Latios",
-    answer_3: "Groudon",
-    answer_4: "Rayquaza",
-    right_answer: 2,
+    "question": "Welche Region ist der Schauplatz der Pokémon-Spiele der zweiten Generation (Gold, Silber, Kristall)?",
+    "answer_1": "Kanto",
+    "answer_2": "Hoenn",
+    "answer_3": "Johto",
+    "answer_4": "Sinnoh",
+    "right_answer": 3
   },
   {
-    question:
-      "Welche Attacke kann nur von Deoxys in seiner Angriffsform erlernt werden?",
-    answer_1: "Hyperstrahl",
-    answer_2: "Konter",
-    answer_3: "Psyschub",
-    answer_4: "Temposchub",
-    right_answer: 4,
+    "question": "Welches Pokémon entwickelt sich aus Pichu (Generation 2)?",
+    "answer_1": "Rattfratz",
+    "answer_2": "Pikachu",
+    "answer_3": "Evoli",
+    "answer_4": "Pii",
+    "right_answer": 2
   },
   {
-    question:
-      "Welches Pokémon ist laut Pokédex-Eintrag das schwerste nicht-legendäre Pokémon der Generation 3?",
-    answer_1: "Stolloss",
-    answer_2: "Walraisa",
-    answer_3: "Wailord",
-    answer_4: "Regirock",
-    right_answer: 3,
+    "question": "Welches Item muss ein Nebulak halten, um sich beim Tausch zu Gengar zu entwickeln?",
+    "answer_1": "Finsterstein",
+    "answer_2": "Abyssplatte",
+    "answer_3": "Scharfklaue",
+    "answer_4": "Keines, es entwickelt sich ohne Item",
+    "right_answer": 4
   },
   {
-    question:
-      "Wie lautet der einzige Typ, den es in Generation 1 zwar gab, der aber keine eigenen Attacken hatte?",
-    answer_1: "Unlicht",
-    answer_2: "Stahl",
-    answer_3: "Drache",
-    answer_4: "Fee",
-    right_answer: 3,
+    "question": "Wer ist der Champ der Pokémon Liga in der Johto-Region (Generation 2)?",
+    "answer_1": "Rot",
+    "answer_2": "Blau",
+    "answer_3": "Lance",
+    "answer_4": "Siegfried",
+    "right_answer": 3
   },
+  {
+    "question": "Wie lautet der Name der Baby-Form von Magmar (Generation 2)?",
+    "answer_1": "Elekid",
+    "answer_2": "Magby",
+    "answer_3": "Kussilla",
+    "answer_4": "Fluffeluff",
+    "right_answer": 2
+  },
+  {
+    "question": "Welche Region ist der Schauplatz der Pokémon-Spiele der dritten Generation (Rubin, Saphir, Smaragd)?",
+    "answer_1": "Johto",
+    "answer_2": "Hoenn",
+    "answer_3": "Sinnoh",
+    "answer_4": "Einall",
+    "right_answer": 2
+  },
+  {
+    "question": "Wie viele Typen-Kombinationen gab es bis zur dritten Generation?",
+    "answer_1": "17",
+    "answer_2": "16",
+    "answer_3": "100",
+    "answer_4": "136",
+    "right_answer": 4
+  },
+  {
+    "question": "Welches der folgenden Pokémon kann die Fähigkeit 'Wassertempo' besitzen?",
+    "answer_1": "Knakrack",
+    "answer_2": "Tohaido",
+    "answer_3": "Glurak",
+    "answer_4": "Letarking",
+    "right_answer": 2
+  },
+  {
+    "question": "Wie entwickelt sich Kokuna zu Bibor (Generation 1)?",
+    "answer_1": "Level 10",
+    "answer_2": "Tausch",
+    "answer_3": "Sonnstein",
+    "answer_4": "Level 7",
+    "right_answer": 1
+  },
+  {
+    "question": "Welches ist das Maskottchen des Teams Rocket in der ersten Generation?",
+    "answer_1": "Mauzi",
+    "answer_2": "Smettbo",
+    "answer_3": "Pikachu",
+    "answer_4": "Woingenau",
+    "right_answer": 1
+  },
+  {
+    "question": "Welches ist das einzige Drachen-Pokémon der ersten Generation (abgesehen von der Entwicklungslinie)?",
+    "answer_1": "Lavados",
+    "answer_2": "Mewtu",
+    "answer_3": "Dragoran",
+    "answer_4": "Garados",
+    "right_answer": 3
+  },
+  {
+    "question": "Welches Pokémon hat die einzigartige Typenkombination Wasser/Boden in Generation 3?",
+    "answer_1": "Sumpex",
+    "answer_2": "Kyogre",
+    "answer_3": "Wailord",
+    "answer_4": "Seewal",
+    "right_answer": 1
+  },
+  {
+    "question": "Wie lautet der Name des Professor in der Hoenn-Region (Generation 3)?",
+    "answer_1": "Professor Eich",
+    "answer_2": "Professor Lind",
+    "answer_3": "Professor Birk",
+    "answer_4": "Professor Eibe",
+    "right_answer": 3
+  },
+  {
+    "question": "Wie viele Pokémon umfasst der Hoenn-Dex (Generation 3)?",
+    "answer_1": "202",
+    "answer_2": "150",
+    "answer_3": "135",
+    "answer_4": "251",
+    "right_answer": 1
+  },
+  {
+    "question": "Welches Pokémon entwickelt sich durch den Donnerstein?",
+    "answer_1": "Vulpix",
+    "answer_2": "Pikachu",
+    "answer_3": "Evoli zu Aquana",
+    "answer_4": "Fukano",
+    "right_answer": 2
+  },
+  {
+    "question": "Welches Pokémon der ersten Generation hat den höchsten Basis-Angriffswert?",
+    "answer_1": "Machomei",
+    "answer_2": "Dragoran",
+    "answer_3": "Mewtu",
+    "answer_4": "Pinsir",
+    "right_answer": 3
+  },
+  {
+    "question": "Wie lautet der Name des Hauptrivalen in den Spielen Pokémon Gold und Silber?",
+    "answer_1": "Blau",
+    "answer_2": "Silber",
+    "answer_3": "Rot",
+    "answer_4": "Gary",
+    "right_answer": 2
+  },
+  {
+    "question": "Welches Item wird benötigt, damit Porygon zu Porygon2 (Generation 2) wird?",
+    "answer_1": "Up-Grade",
+    "answer_2": "Stromisierer",
+    "answer_3": "Metallmantel",
+    "answer_4": "Dubiosdisc",
+    "right_answer": 1
+  },
+  {
+    "question": "Welche Region führte die Fähigkeit 'Schwebe' ein?",
+    "answer_1": "Kanto",
+    "answer_2": "Johto",
+    "answer_3": "Hoenn",
+    "answer_4": "Sinnoh",
+    "right_answer": 3
+  },
+  {
+    "question": "Welches der folgenden Pokémon kann die Fähigkeit 'Innere Kraft' besitzen?",
+    "answer_1": "Guardevoir",
+    "answer_2": "Latios",
+    "answer_3": "Ninjatom",
+    "answer_4": "Heiteira",
+    "right_answer": 4
+  },
+  {
+    "question": "Wie heißt der Kampfstil, der in Generation 3 eingeführt wurde und zwei Pokémon gleichzeitig kämpfen lässt?",
+    "answer_1": "Himmelskampf",
+    "answer_2": "Doppelkampf",
+    "answer_3": "Reihumkampf",
+    "answer_4": "Dreierkampf",
+    "right_answer": 2
+  },
+  {
+    "question": "Welches Pokémon entwickelt sich aus Kindwurm in Generation 3?",
+    "answer_1": "Salamence",
+    "answer_2": "Brutalanda",
+    "answer_3": "Dracomet",
+    "answer_4": "Dratini",
+    "right_answer": 2
+  },
+  {
+    "question": "Welche Attacke ist die Signature-Attacke von Rayquaza?",
+    "answer_1": "Draco Meteor",
+    "answer_2": "Fliegen",
+    "answer_3": "Zenitsturm",
+    "answer_4": "Hyperstrahl",
+    "right_answer": 3
+  },
+  {
+    "question": "Welches legendäre Pokémon der Generation 3 gehört zum 'Äonen-Duo'?",
+    "answer_1": "Jirachi",
+    "answer_2": "Latios",
+    "answer_3": "Deoxys",
+    "answer_4": "Regigigas",
+    "right_answer": 2
+  },
+  {
+    "question": "Wie viele Pokémon umfasste die erste Generation insgesamt (inklusive Mew)?",
+    "answer_1": "150",
+    "answer_2": "151",
+    "answer_3": "100",
+    "answer_4": "251",
+    "right_answer": 2
+  },
+  {
+    "question": "Welche Pokémon-Art ist das Starter-Pokémon 'Endivie'?",
+    "answer_1": "Feuer-Typ",
+    "answer_2": "Pflanzen-Typ",
+    "answer_3": "Wasser-Typ",
+    "answer_4": "Normal-Typ",
+    "right_answer": 2
+  },
+  {
+    "question": "Welches Pokémon der zweiten Generation ist bekannt für die Fähigkeit, seine Statuswerte zu kopieren ('Imitation')?",
+    "answer_1": "Ditto",
+    "answer_2": "Farbeagle",
+    "answer_3": "Porygon2",
+    "answer_4": "Despotar",
+    "right_answer": 2
+  },
+  {
+    "question": "Welche neue Art von Attacken wurde in der dritten Generation eingeführt, die den Status des Gegners verändert?",
+    "answer_1": "Fähigkeitsattacken",
+    "answer_2": "Wetter-Attacken",
+    "answer_3": "Erb-Attacken",
+    "answer_4": "Fokus-Attacken",
+    "right_answer": 2
+  },
+  {
+    "question": "Welches Pokémon ist die Basisform von 'Iksbat' (Generation 2)?",
+    "answer_1": "Zubat",
+    "answer_2": "Golbat",
+    "answer_3": "Kramurx",
+    "answer_4": "Geowaz",
+    "right_answer": 2
+  },
+  {
+    "question": "Welches Item ermöglichte die Entwicklung von Evoli zu Nachtara (Generation 2)?",
+    "answer_1": "Mondstein",
+    "answer_2": "Finsterstein",
+    "answer_3": "Hohe Zuneigung und Level-Up in der Nacht",
+    "answer_4": "Hohe Zuneigung und Level-Up am Tag",
+    "right_answer": 3
+  },
+  {
+    "question": "Welches der folgenden ist kein Mitglied der legendären Regi-Trio der Hoenn-Region?",
+    "answer_1": "Regirock",
+    "answer_2": "Registeel",
+    "answer_3": "Regice",
+    "answer_4": "Regigigas",
+    "right_answer": 4
+  },
+  {
+    "question": "Welches Pokémon der ersten Generation hat die geringste Basis-Verteidigung?",
+    "answer_1": "Pummeluff",
+    "answer_2": "Chaneira",
+    "answer_3": "Flegmon",
+    "answer_4": "Rattfratz",
+    "right_answer": 2
+  },
+  {
+    "question": "Welche Attacke konnte in der ersten Generation den Statuswert 'Initiative' des Gegners senken?",
+    "answer_1": "Bodyguard",
+    "answer_2": "Blubber",
+    "answer_3": "Blitz",
+    "answer_4": "Sandwirbel",
+    "right_answer": 3
+  },
+  {
+    "question": "Welches Pokémon der dritten Generation hat die höchste Basis-Initiative?",
+    "answer_1": "Metagross",
+    "answer_2": "Gewaldro",
+    "answer_3": "Deoxys (Angriffsform)",
+    "answer_4": "Latios",
+    "right_answer": 3
+  }
 ];
 
 let currentQuestion = 0;
 let rightAnswers = 0;
 let tempQuestion = question[currentQuestion];
 let answered = false;
-let success = new Audio("sounds/very-good-sound.mp3");
+let success = new Audio("sounds/success-sound.mp3");
+let goodJob = new Audio("sounds/very-good-sound.mp3");
 
 function init() {
   let numofQuestions = document.getElementById("numberOfAllQuestions");
@@ -140,8 +372,8 @@ function answer(answerId) {
   const key = answerId;
   answered = true;
   buttonDisabled();
-  answerContainer = document.getElementById(answerId);
-  rightContainer = document.getElementById(
+  let answerContainer = document.getElementById(answerId);
+  let rightContainer = document.getElementById(
     "answer_" + tempQuestion.right_answer
   );
   switch (key) {
@@ -159,7 +391,7 @@ function answer(answerId) {
 
 function resetAnswerBackground() {
   for (let index = 1; index <= 4; index++) {
-    tempAnswer = document.getElementById("answer_" + index);
+  let tempAnswer = document.getElementById("answer_" + index);
     tempAnswer.classList.remove("bg-success");
     tempAnswer.classList.remove("bg-danger");
   }
@@ -185,18 +417,21 @@ function showrightAnswers() {
 }
 
 function showResult() {
-  cardContainer = document.getElementById("cardContainer");
+  const cardContainer = document.getElementById("cardContainer");
   cardContainer.innerHTML = "";
-  cardContainer.innerHTML =  resultTemplate();
+  cardContainer.innerHTML = resultTemplate();
   showrightAnswers();
 }
 
 function resultTemplate() {
-    return `<h2>Dein Quiz-Ergebnis</h2>
+  return `<h2>Dein Quiz-Ergebnis</h2>
             <div class="result-body">
                 <p>Du hast <b id="correctAnswers"></b> von <b id="numberOfAllQuestions"></b> Fragen richtig beantwortet!</p>
                 <button type="button" class="btn btn-primary" onclick="restartQuiz()">Quiz neu starten</button>
-                <div class="winner-action-container" id="winnerAction"></div>`;
+                <div class="winner-action-container">
+                  <p class="action" id="resultText">Herzlichen Glückwunsch! Du hast alle Fragen richtig beantwortet!</p>
+                  <img class="action-img" id="resultImg" src="img/all-answers-right-img.gif" alt="#">;
+                </div>`;
 }
 
 function restartQuiz() {
@@ -209,24 +444,44 @@ function restartQuiz() {
 }
 
 function showWinnerAction() {
-    if (rightAnswers === question.length) {
-        let winnerActionContainer = document.getElementById("winnerAction");
-        winnerActionContainer.innerHTML = `<p class="action">Herzlichen Glückwunsch! Du hast alle Fragen richtig beantwortet!</p>
-        <img class="action-img" src="img/all-answers-right-img.gif" alt="#">;`
-    };
-    if (rightAnswers === 0) {
-        let winnerActionContainer = document.getElementById("winnerAction");
-        winnerActionContainer.innerHTML = `<p class="action">Oh nein! Du hast keine Frage richtig beantwortet. Fang an dich mit Pokemon zu beschäftigen!</p>
-        <img class="action-img" src="img/cero-right-answers-img.gif" alt="#">;`
-    };
-    if (rightAnswers <= (question.length * 0,5) && rightAnswers > 0) {
-        let winnerActionContainer = document.getElementById("winnerAction");
-        winnerActionContainer.innerHTML = `<p class="action">Oh nein! Du hast nur ein paar Fragen richtig beantwortet. Fang an dich noch mehr mit Pokemon zu beschäftigen!</p>
-        <img class="action-img" src="img/less-than-half-right-answers-img.gif" alt="#">;`
-    };
-    if (rightAnswers > 5 && rightAnswers < question.length) {
-        let winnerActionContainer = document.getElementById("winnerAction");
-        winnerActionContainer.innerHTML = `<p class="action">Oh Okey! Du hast mehr als die hälfte der Fragen richtig beantwortet. Der beste Pokemon trainer wirst du so halt trotzdem nicht!</p>
-        <img class="action-img" src="img/more-than-half-right-answers-img.gif" alt="#">`
-    };
+  const resultText = document.getElementById("resultText");
+  const resultImg = document.getElementById("resultImg");
+  switch (true) {
+  case rightAnswers === question.length:
+    return allCorrect(resultText, resultImg);
+
+  case rightAnswers === 0:
+    return noneCorrect(resultText, resultImg);
+
+  case rightAnswers <= question.length * 0.5:
+    return someCorrect(resultText, resultImg);
+
+  case rightAnswers < question.length:
+    return mostCorrect(resultText, resultImg);
+}
+}
+
+function allCorrect(resultText, resultImg) {
+  resultText.innerHTML = "Herzlichen Glückwunsch! Du hast alle Fragen richtig beantwortet!";
+    resultImg.src = "img/all-answers-right-img.gif";
+    resultImg.alt = "Alle Antworten richtig";
+    goodJob.play();
+}
+
+function noneCorrect(resultText, resultImg) {
+  resultText.innerHTML = "Oh nein! Du hast keine Frage richtig beantwortet. Versuch es gerne noch einmal!";
+    resultImg.src = "img/no-right-answers-img.gif";
+    resultImg.alt = "Keine richtige Antwort";
+}
+
+function someCorrect(resultText, resultImg) {
+ resultText.innerHTML = "Du hast einige Fragen richtig beantwortet, aber es gibt noch Raum für Verbesserungen. Versuch es gerne noch einmal!";
+    resultImg.src = "img/some-right-answers-img.gif";
+    resultImg.alt = "Einige richtige Antworten";
+}
+
+function mostCorrect(resultText, resultImg) {
+  resultText.innerHTML = "Gute Arbeit! Du hast die meisten Fragen richtig beantwortet. Versuch es gerne noch einmal, um alle richtig zu bekommen!";
+    resultImg.src = "img/good-job-img.gif";
+    resultImg.alt = "Gute Arbeit";
 }
